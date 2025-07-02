7d0dc330e488451064623f0073b94a2a77c3806f
Kishomi99 <kishomini99@gmail.com>
2025-07-03 02:23:32 +0530

    getuser byid and by role api creatation

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
@@ -13,5 +14,10 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
+
+Route::prefix('users')->group(function () {
+    Route::get('/{id}', [UserController::class, 'getUserById']);            // /api/users/{id}
+    Route::get('/role/{role}', [UserController::class, 'getUsersByRole']);  // /api/users/role/{role}
+});
     // Add other protected routes here
 });
